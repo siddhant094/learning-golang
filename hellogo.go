b9a082a439f073eef6c6ea24c7567b1b0ff748a9
Siddhant Pandey <pandeysiddhant21@gmail.com>
2023-12-06 13:59:49 +0530

    starting with go

--- hellogo.go
+++ hellogo.go
@@ -1,38 +1,13 @@
 package main
 
-import (
-	// "bufio"
-	"fmt"
-	// "log"
-	// "os"
-)
+// "bufio"
+
+// "log"
+// "os"
 
 func main() {
-	var name string = "JIIT"
-
-	fmt.Printf("Welcome to %v\n", name)
-
-	// var (
-	// 	i int = 20
-	// 	f float64 = 3.14
-	// )
-
+	
+    // cV1 := 1.5
+    // cV2:= int(cV1)
+    // fmt.println(cV2)
 }
-
-// func calc(num, num2 int) int {
-// 	return num	+num2
-// }
-
-// package main
-
-// // import "gofr.dev/pkg/gofr"
-
-// func main() {
-//     app := gofr.New()
-
-//     app.GET("/hello", func(c *gofr.Context) (interface{}, error) {
-// 		return "Hello GoFr!", nil
-// 	})
-
-//     app.Start()
-// }
